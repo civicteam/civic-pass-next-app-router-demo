6aa761c47c4ab03da5cbb6d2cd84b22cdbf68bc4
dankelleher <dankelleher@yahoo.com>
2024-04-17 13:21:34 +0200

    UI cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,15 @@ import { DynamicWidget, useDynamicContext } from "../lib/dynamic";
 import {CivicPassProvider} from "@/app/context/CivicPassProvider";
 import {IdentityButton} from "@civic/ethereum-gateway-react";
 
-export default async function Home() {
+export default function Home() {
     return (
         <CivicPassProvider>
-            <main className="flex min-h-screen flex-col items-center justify-between p-24">
-                <DynamicWidget/>
-                <IdentityButton/>
-            </main>
+            <div className="flex min-h-screen items-center justify-center">
+                <main className="flex flex-col items-center justify-between p-6 max-h-[200px]">
+                    <DynamicWidget/>
+                    <IdentityButton/>
+                </main>
+            </div>
         </CivicPassProvider>
     )
 };
